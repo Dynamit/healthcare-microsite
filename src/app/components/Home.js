6347028389c477b240557a926b0a4111d3c73cab
Luke Askew <lukeaskew@gmail.com>
2015-07-30 15:02:26 -0400

    add router link to article

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 class Home extends React.Component {
 	render () {
 		return (
-			<div>Home</div>
+			<div>
+				<h1>Home</h1>
+				<Link to="article" params={{ slug: "introduction" }}>Introduction</Link>
+			</div>
 		);
 	}
 };
