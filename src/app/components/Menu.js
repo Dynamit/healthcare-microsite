6274940cd12e9b297be9c7b380f3222e64ff4ea6
Luke Askew <lukeaskew@gmail.com>
2015-08-05 15:36:01 -0400

    add header links

--- src/app/components/Menu.js
+++ src/app/components/Menu.js
@@ -10,44 +10,50 @@ class Menu extends React.Component {
 		let articles = sortBy(this.props.articles, 'date.raw');
 
 		return (
-			<ul className="menu">
-			{this.props.items.map((item, i) => {
-				// iterate through each possible article
-				// if an entry exists, show it's data
-				// else, show the placeholder
-				if (articles[i]) {
+			<div className="menu">
+				<div className="util-links">
+					<a href="#">Get Notified</a>
+					<a href="#">Contact</a>
+				</div>
+				<ul className="menu-list">
+				{this.props.items.map((item, i) => {
+					// iterate through each possible article
+					// if an entry exists, show it's data
+					// else, show the placeholder
+					if (articles[i]) {
 
-					let itemClasses = classNames({
-						'menu-item': true,
-						'is-selected': (this.props.selectedArticle === articles[i].slug)
-					});
+						let itemClasses = classNames({
+							'menu-item': true,
+							'is-selected': (this.props.selectedArticle === articles[i].slug)
+						});
 
-					return (
-						<li key={i} className={itemClasses} onClick={this.props.handleSelectArticle.bind(this, articles[i].slug)}>
-							<div className="menu-item-image" style={{ backgroundImage: `url(/assets/images/${articles[i].thumbnail})` }}></div>
-							<div className="menu-item-content">
-								<a href={`/article/${articles[i].slug}/`}
-									onClick={this.props.handleSelectArticle.bind(this, i)}
-									className="menu-item-title">{articles[i].title}</a>
-								<div className="menu-item-date">{articles[i].date.formatted}</div>
-								<div className="menu-item-placeholder">{item}</div>
-							</div>
-						</li>
-					);
+						return (
+							<li key={i} className={itemClasses} onClick={this.props.handleSelectArticle.bind(this, articles[i].slug)}>
+								<div className="menu-item-image" style={{ backgroundImage: `url(/assets/images/${articles[i].thumbnail})` }}></div>
+								<div className="menu-item-content">
+									<a href={`/article/${articles[i].slug}/`}
+										onClick={this.props.handleSelectArticle.bind(this, i)}
+										className="menu-item-title">{articles[i].title}</a>
+									<div className="menu-item-date">{articles[i].date.formatted}</div>
+									<div className="menu-item-placeholder">{item}</div>
+								</div>
+							</li>
+						);
 
-				} else {
+					} else {
 
-					let placeholder = `<div>${item.split(' ')[0]}</div><div>${item.split(' ')[1]}</div>`;
+						let placeholder = `<div>${item.split(' ')[0]}</div><div>${item.split(' ')[1]}</div>`;
 
-					return (
-						<li key={i} className="menu-item is-unpublished">
-							<div className="menu-item-placeholder" dangerouslySetInnerHTML={{ __html: placeholder }}></div>
-						</li>
-					);
+						return (
+							<li key={i} className="menu-item is-unpublished">
+								<div className="menu-item-placeholder" dangerouslySetInnerHTML={{ __html: placeholder }}></div>
+							</li>
+						);
 
-				}
-			})}
-			</ul>
+					}
+				})}
+				</ul>
+			</div>
 		);
 	}
 
